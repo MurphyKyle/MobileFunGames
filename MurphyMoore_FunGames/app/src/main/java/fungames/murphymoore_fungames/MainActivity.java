e7d849e46582543f50bdfb1c5faf7fcbb7279c51
Kyle Murphy <m.kyle1@yahoo.com>
2018-07-18 11:19:28 -0600

    jsoup and assignment instructions import

--- MurphyMoore_FunGames/app/src/main/java/fungames/murphymoore_fungames/MainActivity.java
+++ MurphyMoore_FunGames/app/src/main/java/fungames/murphymoore_fungames/MainActivity.java
@@ -3,6 +3,17 @@ package fungames.murphymoore_fungames;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+// Load the web content and images from:
+// https://www.pcauthority.com.au/News/170181,top-10-computer-games-of-all-time.aspx            20pts
+// Search the downloaded content for the top 10 games                                           20pts
+// Put the names and images in a list                                                           10pts
+// The user will have 4 options on each screen to guess the right answer                        20pts
+// Show a Toast if the answer is correct or incorrect                                           5pt
+// Stay on the current image if the answer is incorrect, otherwise go to the next question.     25pt
+
+
+
 public class MainActivity extends AppCompatActivity {
 	
 	@Override
@@ -10,4 +21,10 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
+	
+	
+	
+	
+	
+	
 }
