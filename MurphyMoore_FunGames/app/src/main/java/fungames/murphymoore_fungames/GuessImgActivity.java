45c4a330581d48b81934905e5b284d2ed2ccd234
Christian Moore <codeyellow97@gmail.com>
2018-07-23 11:48:41 -0600

    Broken Code

--- MurphyMoore_FunGames/app/src/main/java/fungames/murphymoore_fungames/GuessImgActivity.java
+++ MurphyMoore_FunGames/app/src/main/java/fungames/murphymoore_fungames/GuessImgActivity.java
@@ -4,6 +4,8 @@ import android.media.TimedText;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -16,21 +18,23 @@ public class GuessImgActivity extends AppCompatActivity {
 	@Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guess_img);
         ArrayList<String> imgs =  getIntent().getStringArrayListExtra("imgs");
         ArrayList<String> titles = getIntent().getStringArrayListExtra("titles");
         makeGameEntries(imgs, titles);
-
+        TextView t = (TextView) findViewById(R.id.random);
+        t.setText(getGameEntries().get(0).getCorrectAnswer());
     }
-	
+
 	public static ArrayList<GameEntry> getGameEntries() {
 		return gameEntries;
 	}
-	
-	public static void setGameEntries(ArrayList<GameEntry> gameEntries) {
-		if (gameEntries != null ) {
-			GuessImgActivity.gameEntries = gameEntries;
-		}
-	}
+//
+//	public static void setGameEntries(ArrayList<GameEntry> gameEntries) {
+//		if (gameEntries != null ) {
+//			GuessImgActivity.gameEntries = gameEntries;
+//		}
+//	}
 
 
 
@@ -45,15 +49,14 @@ public class GuessImgActivity extends AppCompatActivity {
     private ArrayList<String> makeRandomTitleList(String correctAnswer){
         String[] randomTitles = new String[]{"Halo", "Star Craft", "Online Poker", "Solitaire", "Go Fish"};
         ArrayList<String> result = new ArrayList<>();
-        int i = 3;
+        int i = 0;
         Random rand = new Random();
-        while(i < 3){
+        while(i < 4){
             int index = rand.nextInt(randomTitles.length);
-            result.add(randomTitles[index]);
             if(result.contains(randomTitles[index])){
                 result.remove(result.size()-1);
-            }
-            else{
+            }else{
+                result.add(randomTitles[index]);
                 i++;
             }
         }
